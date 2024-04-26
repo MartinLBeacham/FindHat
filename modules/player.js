ed7872859867c8df08adcf2381ce7cba5d468c83
Martin Beacham <martinlutherbeacham@gmail.com>
2024-04-26 16:21:08 -0400

    remove old comments

--- modules/player.js
+++ modules/player.js
@@ -62,68 +62,7 @@ class Player {
         break;
     }
 
-    // switch (moveDirection) {
-    //   case "forward":
-    //     if (this._direction === "B") {
-    //       this._position[0] = row - 1;
-    //     } else if ((this._direction = "F")) {
-    //       this._position[0] = row + 1;
-    //     } else if ((this._direction = "R")) {
-    //       this._position[1] = col + 1;
-    //     } else {
-    //       this._position[1] = col - 1;
-    //     }
-    //     break;
-    //   case "backward":
-    //     if (this._direction === "B") {
-    //       this._position[0] = row + 1;
-    //       this._direction = "F";
-    //     } else if ((this._direction = "F")) {
-    //       this._position[0] = row - 1;
-    //       this._direction = "B";
-    //     } else if ((this._direction = "R")) {
-    //       this._position[1] = col - 1;
-    //       this._direction = "L";
-    //     } else {
-    //       this._position[1] = col + 1;
-    //       this._direction = "R";
-    //     }
-    //     break;
-    //   case "right":
-    //     if (this._direction === "B") {
-    //       this._position[1] = col - 1;
-    //       this._direction = "R";
-    //     } else if ((this._direction = "F")) {
-    //       this._position[1] = col + 1;
-    //       this._direction = "L";
-    //     } else if ((this._direction = "R")) {
-    //       this._position[0] = row + 1;
-    //       this._direction = "F";
-    //     } else {
-    //       this._position[0] = row - 1;
-    //       this._direction = "B";
-    //     }
-    //     break;
-    //   case "left":
-    //     if (this._direction === "B") {
-    //       this._position[1] = col + 1;
-    //       this._direction = "L";
-    //     } else if ((this._direction = "F")) {
-    //       this._position[1] = col - 1;
-    //       this._direction = "R";
-    //     } else if ((this._direction = "R")) {
-    //       this._position[0] = row - 1;
-    //       this._direction = "B";
-    //     } else {
-    //       this._position[0] = row + 1;
-    //       this._direction = "F";
-    //     }
-    //     break;
-    //   default:
-    //     console.log("Enter 'forward', 'backward', 'left', 'right'");
-    //     return null;
-    // }
-  }
+ }
 }
 
 module.exports = Player;
